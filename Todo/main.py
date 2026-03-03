2850d6c03aae87fa54458aef012847a47330bd78
Farhan Khan <mdfarhanahmadkhan7786@gmail.com>
2026-03-03 17:39:30 +0530

    created Post request method using pydantic for parsing in todo app

--- Todo/main.py
+++ Todo/main.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI, Depends, HTTPException, status, Path
 import models
 from models import Todos
 from database import engine, SessionLocal
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
@@ -18,6 +19,12 @@ def get_db():
 
 db_dependency = Annotated[Session, Depends(get_db)]
 
+class TodoRequest(BaseModel):
+    title: str = Field(min_length= 3)
+    description: str = Field(min_length= 5, max_length=100)
+    priority: int = Field(gt=0, lt= 6)
+    complete: bool 
+
 @app.get("/", status_code= status.HTTP_200_OK)
 # async def read_all(db: Annotated[Session, Depends(get_db)]):
 async def read_all(db: db_dependency):
@@ -29,3 +36,10 @@ async def read_todo(db: db_dependency, todo_id: int = Path(gt= 0)):
     if todo_model is not None:
         return todo_model
     raise HTTPException(status_code=404, detail='To do not found.')
+
+@app.post("/todo", status_code=status.HTTP_201_CREATED)
+async def create_todo(db: db_dependency, todo_request: TodoRequest):
+    todo_model = Todos(**todo_request.model_dump())
+
+    db.add(todo_model)
+    db.commit()
