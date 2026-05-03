4a731b852cac82836fe64f2c8a0593f8c1930c7d
Farhan Khan <mdfarhanahmadkhan7786@gmail.com>
2026-05-03 23:05:24 +0530

    created routers to deal with complexity did authentication and authorization did JWT

--- Todo/models.py
+++ Todo/models.py
@@ -1,5 +1,20 @@
 from database import Base
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+
+class Users(Base):
+    __tablename__ = 'user'
+    
+    id = Column(Integer, primary_key= True)
+    email = Column(String, unique= True)
+    username = Column(String, unique= True)
+    first_name = Column(String)
+    last_name = Column(String)
+    hashed_password = Column(String)
+    is_active = Column(String)
+    complete = Column(Boolean, default= True)
+    role = Column(String)
+
+
 
 class Todos(Base):
     __tablename__ = 'todos'
@@ -9,3 +24,4 @@ class Todos(Base):
     description = Column(String)
     priority = Column(Integer)
     complete = Column(Boolean, default = False)
+    owner_id = Column(Integer, ForeignKey("user.id"))
